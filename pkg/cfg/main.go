4ef348e660d52ad1b43d16fec8a134c893710975
kts of kettek (POWERQWACK) <kts@kettek.net>
2022-05-23 20:47:37 -0700

    Add FromYaml

--- pkg/cfg/main.go
+++ pkg/cfg/main.go
@@ -1,10 +1,22 @@
 package cfg
 
 import (
+	"os"
 	"pingmon/pkg/pinger"
+
+	"gopkg.in/yaml.v3"
 )
 
 type Config struct {
 	Targets []pinger.Target `yaml:"targets"`
 	Address string          `yaml:"address"`
+}
+
+func (c *Config) FromYAML(p string) error {
+	b, err := os.ReadFile(p)
+	if err != nil {
+		return err
+	}
+
+	return yaml.Unmarshal(b, &c)
 }
