99ce0ae80ea246d443c25feaf17d0fc98ffc5632
kts of kettek <kts@kettek.net>
2022-05-24 04:17:41 -0700

    Allow non-prefixed target; default to tcp :80

--- pkg/backend/target.go
+++ pkg/backend/target.go
@@ -21,19 +21,25 @@ func (t *Target) UnmarshalYAML(unmarshal func(interface{}) error) error {
 	}
 
 	parts := strings.SplitN(str, ":", 3)
-	if len(parts) >= 1 {
-		t.Method = parts[0]
-	}
-	if len(parts) >= 2 {
-		t.Address = parts[1]
-	}
-	if len(parts) >= 3 {
-		t.Port, _ = strconv.Atoi(parts[2])
+	if len(parts) == 1 {
+		t.Method = "tcp"
+		t.Address = parts[0]
+		t.Port = 80
 	} else {
-		if t.Method == "tcp" {
-			t.Port = 80
-		} else if t.Method == "udp" {
-			t.Port = 53
+		if len(parts) >= 1 {
+			t.Method = parts[0]
+		}
+		if len(parts) >= 2 {
+			t.Address = parts[1]
+		}
+		if len(parts) >= 3 {
+			t.Port, _ = strconv.Atoi(parts[2])
+		} else {
+			if t.Method == "tcp" {
+				t.Port = 80
+			} else if t.Method == "udp" {
+				t.Port = 53
+			}
 		}
 	}
 
