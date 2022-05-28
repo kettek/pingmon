de69a9cddc5d853ca2d0d1527c62fea0bd92fa94
kts of kettek <kts@kettek.net>
2022-05-28 07:09:20 -0700

    Add system tray for the fun of it

---
+++ cmd/pingmon-systray/main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"pingmon/pkg/core"
+	"sync"
+	"time"
+
+	"github.com/getlantern/systray"
+)
+
+var c *core.Config
+var entries []*systray.MenuItem
+var lock sync.Mutex
+
+func main() {
+	c = &core.DefaultConfig
+
+	if err := c.FromYAML("cfg.yml"); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	if c.Targets == nil || len(*c.Targets) == 0 {
+		fmt.Println(errors.New("no targets"))
+		os.Exit(1)
+	}
+
+	systray.Run(onReady, onExit)
+}
+
+func refreshEntries() {
+	lock.Lock()
+	defer lock.Unlock()
+	for i, t := range *c.Targets {
+		title := fmt.Sprintf("%s %s:%s:%d", t.Status, t.Method, t.Address, t.Port)
+		if t.Status == "online" {
+			title += fmt.Sprintf(" @ %.3fms", t.Delay/1024)
+			entries[i].Check()
+		} else {
+			entries[i].Uncheck()
+		}
+		entries[i].SetTitle(title)
+		entries[i].SetTooltip(fmt.Sprintf("%s: %s", t.Status, t.ExtendedStatus))
+	}
+}
+
+func onReady() {
+	var prefix, name, suffix string
+	if c.Title.Prefix != nil {
+		prefix = *c.Title.Prefix
+	}
+	if c.Title.Name != nil {
+		name = *c.Title.Name
+	}
+	if c.Title.Suffix != nil {
+		suffix = *c.Title.Suffix
+	}
+
+	systray.SetTitle(fmt.Sprintf("%s%s%s", prefix, name, suffix))
+
+	// Add entries for all our targets.
+	for range *c.Targets {
+		entries = append(entries, systray.AddMenuItem("", ""))
+	}
+	systray.AddSeparator()
+	// Add our normal menu items.
+	refresh := systray.AddMenuItem("Refresh", "Refreshes all targets")
+	go func() {
+		for {
+			<-refresh.ClickedCh
+			core.RefreshTargets(c)
+			refreshEntries()
+		}
+	}()
+	quit := systray.AddMenuItem("Quit", "Quit")
+	go func() {
+		for {
+			<-quit.ClickedCh
+			systray.Quit()
+		}
+	}()
+
+	// Update and sync.
+	core.RefreshTargets(c)
+	refreshEntries()
+
+	// Start our refresh timer.
+	go func() {
+		for {
+			core.RefreshTargets(c)
+			refreshEntries()
+			select {
+			case <-time.After(time.Duration(c.Rate) * time.Second):
+			}
+		}
+	}()
+}
+
+func onExit() {
+	// dunno
+}
