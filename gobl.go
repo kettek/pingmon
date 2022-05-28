adb17f9ca54c56f50190b7756a795b0e76360e4a
kts of kettek <kts@kettek.net>
2022-05-28 06:42:06 -0700

    Add CLI tasks and reorg

--- gobl.go
+++ gobl.go
@@ -5,28 +5,39 @@ import (
 )
 
 func main() {
+	// Backend
 	Task("buildBackend").
 		Exec("go", "build", "-v", "./cmd/pingmon")
-
 	Task("runBackend").
 		Exec("./pingmon")
-
-	Task("buildFrontend").
-		Chdir("pkg/frontend").
-		Exec("npm", "i").
-		Exec("npm", "run", "build")
-
 	Task("watchBackend").
-		Watch("cmd/pingmon/*", "pkg/backend/*.go").
+		Watch("cmd/pingmon/*", "pkg/backend/*.go", "pkg/core/*.go").
 		Signaler(SigQuit).
 		Run("buildBackend").
 		Run("runBackend")
 
+	// Frontend
+	Task("buildFrontend").
+		Chdir("pkg/frontend").
+		Exec("npm", "i").
+		Exec("npm", "run", "build")
 	Task("watchFrontend").
 		Watch("pkg/frontend/src/*.*").
 		Signaler(SigQuit).
 		Run("buildFrontend")
 
+	// CLI
+	Task("buildCLI").
+		Exec("go", "build", "-v", "./cmd/pingmon-cli")
+	Task("runCLI").
+		Exec("./pingmon-cli")
+	Task("watchCLI").
+		Watch("cmd/pingmon-cli/*", "pkg/core/*").
+		Signaler(SigQuit).
+		Run("buildCLI").
+		Run("runCLI")
+
+	// Installers
 	Task("installSystemdUnit").
 		Exec("mkdir", "-p", "/opt/pingmon/public").
 		Exec("cp", "pingmon", "/opt/pingmon/").
